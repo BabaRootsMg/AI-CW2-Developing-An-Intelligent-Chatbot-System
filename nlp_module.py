12bf5f887e82ed0c30f24bc4aab8d9630d1b7d5a
John <johnwiam@icloud.com>
2025-05-07 15:31:49 +0100

    done

--- nlp_module.py
+++ nlp_module.py
@@ -1,10 +1,4 @@
-"""
-nlp_module.py
--------------
-This module processes and interprets user input using Natural Language Processing (NLP).
-It identifies user intent (e.g., ticket search or delay query) and extracts key information
-(e.g., station names, dates, times) to support chatbot decision-making.
-"""
+
 """
 nlp_module.py
 --------------
